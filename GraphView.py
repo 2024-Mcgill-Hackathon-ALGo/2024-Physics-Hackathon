a24a64211aa2162f893f4cf86604acf1690fa54a
Graciela Nouga <gracielaruthcarolenouga@gmail.com>
2024-11-16 16:26:28 -0500

    Modification du  background et couleur des cercles

--- GraphView.py
+++ GraphView.py
@@ -5,21 +5,31 @@ from MainMenu import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def view_graph():
+    arcade.draw_lrtb_rectangle_filled((SCREEN_WIDTH / 8) * 5, SCREEN_WIDTH, SCREEN_HEIGHT, 0,
+                                       arcade.color.GRAY)
     arcade.draw_lrtb_rectangle_outline((SCREEN_WIDTH / 8) * 5, SCREEN_WIDTH, SCREEN_HEIGHT, 0,
                                        arcade.color.GREEN)
     arcade.draw_line((SCREEN_WIDTH / 8) * 5, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2, arcade.color.GREEN)
 
 def view_circle():
+    arcade.draw_circle_filled(1400, 900, 20,
+                              arcade.color.YELLOW)
+    arcade.draw_line(1400, 800, 1400 , 880, arcade.color.ORANGE_RED,2)
     arcade.draw_circle_outline(1400, 900, 20,
-                              arcade.color.GREEN)
+                              arcade.color.ORANGE_RED)
+    arcade.draw_circle_filled(1400, 800, 20,
+                              arcade.color.YELLOW)
     arcade.draw_circle_outline(1400, 800, 20,
-                              arcade.color.GREEN)
+                              arcade.color.ORANGE_RED)
+    arcade.draw_circle_filled(1400, 400, 20,
+                              arcade.color.YELLOW)
     arcade.draw_circle_outline(1400, 400, 20,
-                              arcade.color.GREEN)
+                              arcade.color.ORANGE_RED)
     arcade.draw_circle_outline(900, 700, 10,
                               arcade.color.GREEN)
     arcade.draw_circle_outline(700, 700, 30,
                               arcade.color.GREEN)
+
 
 
 if __name__ == '__main__':
