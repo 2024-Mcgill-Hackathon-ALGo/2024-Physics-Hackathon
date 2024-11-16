63bcded101416854592eb35d0e996fc0b45b4b5e
Dumitru Gliga <anagliga31@gmail.com>
2024-11-15 21:59:47 -0500

    created element class

---
+++ Element.py
@@ -0,0 +1,22 @@
+class Element:
+    # region constructeurs
+    def __init__(self, symbol, name, atomic_number, atomic_weight):
+        self.symbol = symbol
+        self.name = name
+        self.atomic_number = atomic_number
+        self.atomic_weight = atomic_weight
+    
+    def __init__(self, other):
+        self.symbol = other.symbol
+        self.name = other.name
+        self.atomic_number = other.atomic_number
+        self.atomic_weight = other.atomic_weight  
+    
+    # endregion constructeurs 
+             
+    # region méthodes
+    
+    def equals(self, other):
+        return self.symbol == other.symbol and self.name == other.name and self.atomic_number == other.atomic_number and self.atomic_weight == other.atomic_weight
+    
+    # endregion méthodes
