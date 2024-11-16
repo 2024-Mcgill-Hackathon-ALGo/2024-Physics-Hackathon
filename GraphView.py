d4692f2317a811c6b584afa3bd65ae4c96e6ff9b
Graciela Nouga <gracielaruthcarolenouga@gmail.com>
2024-11-16 15:14:28 -0500

    Ajoût des cercles du graphe

--- GraphView.py
+++ GraphView.py
@@ -1,13 +1,25 @@
 from arcade import Window
 import arcade
+import arcade.color
 from MainMenu import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def view_graph():
-    arcade.draw_lrtb_rectangle_outline((SCREEN_WIDTH / 8) * 7, SCREEN_WIDTH, SCREEN_HEIGHT, 0,
+    arcade.draw_lrtb_rectangle_outline((SCREEN_WIDTH / 8) * 5, SCREEN_WIDTH, SCREEN_HEIGHT, 0,
                                        arcade.color.GREEN)
-    arcade.draw_line((SCREEN_WIDTH / 8) * 7, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2, arcade.color.GREEN)
+    arcade.draw_line((SCREEN_WIDTH / 8) * 5, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2, arcade.color.GREEN)
 
+def view_circle():
+    arcade.draw_circle_outline(1400, 900, 20,
+                              arcade.color.GREEN)
+    arcade.draw_circle_outline(1400, 800, 20,
+                              arcade.color.GREEN)
+    arcade.draw_circle_outline(1400, 400, 20,
+                              arcade.color.GREEN)
+    arcade.draw_circle_outline(900, 700, 10,
+                              arcade.color.GREEN)
+    arcade.draw_circle_outline(700, 700, 30,
+                              arcade.color.GREEN)
 
 
 if __name__ == '__main__':
@@ -20,15 +32,17 @@ if __name__ == '__main__':
 
         def on_draw(self):
             self.clear()
-            arcade.start_render()
+            # arcade.start_render()
             view_graph()
-            arcade.finish_render()
+            view_circle()
+            # arcade.finish_render()
+            
 
 
     def main():
         window = Win()
         window.setup()
-
+        
         arcade.run()
 
 
