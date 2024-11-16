8b808ea4d1daa7d1fe9368e539d76047d0af9a81
Yohan Thibault <y2thibaultgm@gmail.com>
2024-11-16 12:19:21 -0500

    Ajout de la base

--- GraphView.py
+++ GraphView.py
@@ -1,25 +1,33 @@
 from arcade import Window
 import arcade
+from MainMenu import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def view_graph():
-    arcade.draw_lrtb_rectangle_outline(100, 200, 300, 200, color=(0, 255, 0))
+    arcade.draw_lrtb_rectangle_outline((SCREEN_WIDTH / 8) * 7, SCREEN_WIDTH, SCREEN_HEIGHT, 0,
+                                       arcade.color.GREEN)
+    arcade.draw_line((SCREEN_WIDTH / 8) * 7, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2, arcade.color.GREEN)
+
 
 
 if __name__ == '__main__':
     class Win(Window):
         def __init__(self):
-            super().__init__()
+            super().__init__(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 
         def setup(self):
             self.background_color = (0, 0, 0)
 
         def on_draw(self):
+            self.clear()
+            arcade.start_render()
             view_graph()
+            arcade.finish_render()
 
 
     def main():
         window = Win()
+        window.setup()
 
         arcade.run()
 
